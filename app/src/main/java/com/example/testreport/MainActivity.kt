fcd44740bf64262283b6296bf994e03bdecbf5df
prateekcode <codes.prateek@gmail.com>
2021-03-15 11:02:14 +0530

    :zap: Initial commit

---
+++ app/src/main/java/com/example/testreport/MainActivity.kt
@@ -0,0 +1,333 @@
+package com.example.testreport
+
+import android.Manifest
+import android.graphics.*
+import android.graphics.pdf.PdfDocument
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.os.Handler
+import android.text.TextPaint
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.example.testreport.helper.PermissionHelper
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    private var isGenerating = false
+    private lateinit var bitmap: Bitmap
+    private lateinit var scaledBitmap: Bitmap
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        bitmap = BitmapFactory.decodeResource(resources, R.drawable.logonew)
+        scaledBitmap = Bitmap.createScaledBitmap(bitmap, 50, 17, false)
+
+        permissionCheck(false)
+        buttonSavePdf.setOnClickListener {
+            permissionCheck(true)
+            if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                createNewPdf()
+            }
+        }
+    }
+
+    private fun permissionCheck(check: Boolean) {
+        val permissionHelper = PermissionHelper(
+            this,
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ),
+            100
+        )
+        permissionHelper.denied {
+            if (it) {
+                Log.d("Permission check", "Permission denied by system")
+                Toast.makeText(this, "No permission granted", Toast.LENGTH_SHORT).show()
+                permissionHelper.openAppDetailsActivity()
+            } else {
+                Toast.makeText(this, "No permission granted", Toast.LENGTH_SHORT).show()
+                Log.d("Permission check", "Permission denied")
+            }
+        }
+        permissionHelper.requestAll {
+            Log.d("Permission check", "All permission granted")
+
+            if (!isGenerating && check) {
+                isGenerating = true
+
+                val handler = Handler()
+                val runnable = Runnable {
+                    //to avoid multiple generation at the same time. Set isGenerating = false on some delay
+                    isGenerating = false
+                }
+                handler.postDelayed(runnable, 2000)
+                //  Toast.makeText(this, "Permission Has Been Granted", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        permissionHelper.requestIndividual {
+            Log.d("Permission check", "Individual Permission Granted")
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun createNewPdf() {
+        val pdfDocument = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            PdfDocument()
+        } else {
+            TODO("VERSION.SDK_INT < KITKAT")
+        }
+        val myPageInfo = PdfDocument.PageInfo.Builder(250, 400, 1).create()
+        val myPage1 = pdfDocument.startPage(myPageInfo) as PdfDocument.Page
+
+        val paint = Paint()
+        val canvas = myPage1.canvas as Canvas
+
+
+        //Start of Top Header
+        paint.color = getColor(R.color.extra_dark_blue)
+        paint.typeface = Typeface.create("Arial", Typeface.BOLD)
+        paint.textSize = 14F
+        canvas.drawText("Health Choice Clinic", 80F, 25F, paint)
+
+        paint.typeface = Typeface.create("Arial", Typeface.NORMAL)
+        paint.color = getColor(R.color.black)
+        paint.textSize = 5F
+        canvas.drawText("8th floor Suratwala Mark Plazzo, Hinjawadi Pune", 80F, 35F, paint)
+
+        canvas.drawLine(194F, 30F, 194F, 42F, paint)
+        paint.typeface = Typeface.create("Roboto", Typeface.NORMAL)
+        paint.textSize = 5F
+        canvas.drawText("9452451241", 198F, 35F, paint)
+        canvas.drawText("8245121416", 198F, 42F, paint)
+
+        paint.color = getColor(R.color.black)
+        paint.textSize = 5F
+        canvas.drawText("Maharastra, 41007", 80F, 40F, paint)
+
+        canvas.drawBitmap(scaledBitmap, 10f, 20f, paint)
+        //End of Top Header
+
+        //Start of Patient Detail Section
+        paint.color = getColor(R.color.purple_700)
+        canvas.drawRect(0F, 50F, 300F, 110F, paint)
+        val textPaintPatient = TextPaint()
+        textPaintPatient.typeface = Typeface.create("Roboto", Typeface.BOLD)
+        textPaintPatient.color = getColor(R.color.white)
+        textPaintPatient.textSize = 6F
+        canvas.drawText("Patient Name   :", 10F, 65F,textPaintPatient)
+        canvas.drawText("Age & Gender   :", 10F, 75F,textPaintPatient)
+        canvas.drawText("Referred by      :", 10F, 85F,textPaintPatient)
+        canvas.drawText("Sample Type    :", 10F, 95F,textPaintPatient)
+
+        textPaintPatient.typeface = Typeface.create("Roboto", Typeface.NORMAL)
+        canvas.drawText("Patient Name", 60F, 65F,textPaintPatient)
+        canvas.drawText("27 Year/Male", 60F, 75F,textPaintPatient)
+        canvas.drawText("Dr. Aditya Biswas", 60F, 85F,textPaintPatient)
+        canvas.drawText("EDTA Blood", 60F, 95F,textPaintPatient)
+
+        paint.color = getColor(R.color.white)
+        val midX = (canvas.width/2).toFloat()
+        canvas.drawLine(midX, 50F, midX, 110F, paint)
+
+        textPaintPatient.typeface = Typeface.create("Roboto", Typeface.BOLD)
+        canvas.drawText("Authorization Date   :", midX+10F, 65F,textPaintPatient)
+        canvas.drawText("Report Date   :", midX+10F, 75F,textPaintPatient)
+        canvas.drawText("Patient ID      :", midX+10F, 85F,textPaintPatient)
+
+        textPaintPatient.typeface = Typeface.create("Roboto", Typeface.NORMAL)
+        canvas.drawText("03/03/2021   10:30 am", midX+70, 65F,textPaintPatient)
+        canvas.drawText("03/03/2021   10:30 am", midX+70, 75F,textPaintPatient)
+        canvas.drawText("7653", midX+70, 85F,textPaintPatient)
+        //End of the Patient Detail
+
+
+        //Table of Test
+        paint.color = getColor(R.color.dark_blue)
+        canvas.drawRect(10F, 115F, 80F, 125F, paint)
+        textPaintPatient.typeface = Typeface.create("Roboto", Typeface.BOLD)
+        canvas.drawText("Parameter", 20F, 122F, textPaintPatient)
+
+        canvas.drawRect(83F, 115F, 123F, 125F, paint)
+        canvas.drawText("Results", 88F, 122F, textPaintPatient)
+
+        canvas.drawRect(126F, 115F, 162F, 125F, paint)
+        canvas.drawText("Units", 130F, 122F, textPaintPatient)
+
+        canvas.drawRect(165F, 115F, 202F, 125F, paint)
+        canvas.drawText("Ref. Range", 168F, 122F, textPaintPatient)
+
+        canvas.drawRect(205F, 115F, 237F, 125F, paint)
+        canvas.drawText("Barcode", 209F, 122F, textPaintPatient)
+
+        paint.color = getColor(R.color.blue_light)
+        canvas.drawRect(10F, 125F, 237F, 135F, paint)
+        textPaintPatient.color = getColor(R.color.black)
+        canvas.drawText("Complete Blood Count", 95F, 132F, textPaintPatient)
+
+        paint.color = getColor(R.color.grey)
+        canvas.drawRect(10F, 135F, 237F, 145F, paint)
+        textPaintPatient.color = getColor(R.color.black)
+        textPaintPatient.typeface = Typeface.create("Roboto", Typeface.NORMAL)
+        canvas.drawText("Anaemia", 115F, 142F, textPaintPatient)
+
+        //Parameter Content
+        textPaintPatient.typeface = Typeface.create("Arial", Typeface.BOLD)
+        canvas.drawText("Hgb Haemoglobin", 20F, 157F, textPaintPatient)
+        canvas.drawText("Hct Hematocrit", 20F, 167F, textPaintPatient)
+        canvas.drawText("Hct/Hgb (Calculated)", 20F, 177F, textPaintPatient)
+        canvas.drawText("MCHC (Calculated)", 20F, 187F, textPaintPatient)
+        canvas.drawText("Hct Hematocrit", 20F, 197F, textPaintPatient)
+        canvas.drawText("Hct/Hgb (Calculated)", 20F, 207F, textPaintPatient)
+
+        //Results Counts
+        textPaintPatient.typeface = Typeface.create("Roboto", Typeface.NORMAL)
+        canvas.drawText(":   15", 88F, 157F, textPaintPatient)
+        canvas.drawText(":   45", 88F, 167F, textPaintPatient)
+        canvas.drawText(":   2.7", 88F, 177F, textPaintPatient)
+        canvas.drawText(":   35", 88F, 187F, textPaintPatient)
+        canvas.drawText(":   45", 88F, 197F, textPaintPatient)
+        canvas.drawText(":   2.7", 88F, 207F, textPaintPatient)
+
+        //Units
+        textPaintPatient.typeface = Typeface.create("Roboto", Typeface.NORMAL)
+        canvas.drawText("g/dL", 130F, 157F, textPaintPatient)
+        canvas.drawText("%", 130F, 167F, textPaintPatient)
+        canvas.drawText("", 130F, 177F, textPaintPatient)
+        canvas.drawText("g/dL", 130F, 187F, textPaintPatient)
+        canvas.drawText("%", 130F, 197F, textPaintPatient)
+        canvas.drawText("", 130F, 207F, textPaintPatient)
+
+        //Ref. Range
+        textPaintPatient.typeface = Typeface.create("Roboto", Typeface.NORMAL)
+        canvas.drawText("14-18", 168F, 157F, textPaintPatient)
+        canvas.drawText("45-54", 168F, 167F, textPaintPatient)
+        canvas.drawText("2.5-3.2", 168F, 177F, textPaintPatient)
+        canvas.drawText("32.5-36", 168F, 187F, textPaintPatient)
+        canvas.drawText("42-54", 168F, 197F, textPaintPatient)
+        canvas.drawText("2.5-3.2", 168F, 207F, textPaintPatient)
+
+        //Total & Differencial Count
+        paint.color = getColor(R.color.grey)
+        canvas.drawRect(10F, 217F, 237F, 227F, paint)
+        textPaintPatient.color = getColor(R.color.black)
+        textPaintPatient.typeface = Typeface.create("Roboto", Typeface.NORMAL)
+        canvas.drawText("Total & Differencial Count", 95F, 224F, textPaintPatient)
+
+        //Parameter Content
+        textPaintPatient.typeface = Typeface.create("Arial", Typeface.BOLD)
+        canvas.drawText("White Blood Cell count", 20F, 239F, textPaintPatient)
+        canvas.drawText("Neutrophill percent", 20F, 249F, textPaintPatient)
+        canvas.drawText("Lymphocyte percent", 20F, 259F, textPaintPatient)
+        canvas.drawText("Monocyte percent", 20F, 269F, textPaintPatient)
+        canvas.drawText("Eosinophil percent", 20F, 279F, textPaintPatient)
+        canvas.drawText("Basophil percent", 20F, 289F, textPaintPatient)
+        canvas.drawText("Neutrophill absolute number", 20F, 299F, textPaintPatient)
+        canvas.drawText("Lymphocyte absolute number", 20F, 309F, textPaintPatient)
+        canvas.drawText("Monocyte absolute number", 20F, 319F, textPaintPatient)
+        canvas.drawText("Eosinophill absoulte number", 20F, 329F, textPaintPatient)
+
+        //Results Counts
+        textPaintPatient.typeface = Typeface.create("Roboto", Typeface.NORMAL)
+        canvas.drawText(":   15", 90F, 239F, textPaintPatient)
+        canvas.drawText(":   45", 90F, 249F, textPaintPatient)
+        canvas.drawText(":   2.7", 90F, 259F, textPaintPatient)
+        canvas.drawText(":   35", 90F, 269F, textPaintPatient)
+        canvas.drawText(":   45", 90F, 279F, textPaintPatient)
+        canvas.drawText(":   2.7", 90F, 289F, textPaintPatient)
+        canvas.drawText(":   2.7", 95F, 299F, textPaintPatient)
+        canvas.drawText(":   35", 95F, 309F, textPaintPatient)
+        canvas.drawText(":   45", 95F, 319F, textPaintPatient)
+        canvas.drawText(":   2.7", 95F, 329F, textPaintPatient)
+
+        //Units
+        textPaintPatient.typeface = Typeface.create("Roboto", Typeface.NORMAL)
+        canvas.drawText("10^9/L", 130F, 239F, textPaintPatient)
+        canvas.drawText("%", 130F, 249F, textPaintPatient)
+        canvas.drawText("%", 130F, 259F, textPaintPatient)
+        canvas.drawText("%", 130F, 269F, textPaintPatient)
+        canvas.drawText("%", 130F, 279F, textPaintPatient)
+        canvas.drawText("%", 130F, 289F, textPaintPatient)
+        canvas.drawText("10^9/L", 130F, 299F, textPaintPatient)
+        canvas.drawText("10^9/L", 130F, 309F, textPaintPatient)
+        canvas.drawText("10^9/L", 130F, 319F, textPaintPatient)
+        canvas.drawText("10^9/L", 130F, 329F, textPaintPatient)
+
+        //Ref. Range
+        textPaintPatient.typeface = Typeface.create("Roboto", Typeface.NORMAL)
+        canvas.drawText("14-18", 168F, 239F, textPaintPatient)
+        canvas.drawText("45-54", 168F, 249F, textPaintPatient)
+        canvas.drawText("2.5-3.2", 168F, 259F, textPaintPatient)
+        canvas.drawText("32.5-36", 168F, 269F, textPaintPatient)
+        canvas.drawText("42-54", 168F, 279F, textPaintPatient)
+        canvas.drawText("2.5-3.2", 168F, 289F, textPaintPatient)
+        canvas.drawText("2.5-3.2", 168F, 299F, textPaintPatient)
+        canvas.drawText("32.5-36", 168F, 309F, textPaintPatient)
+        canvas.drawText("42-54", 168F, 319F, textPaintPatient)
+        canvas.drawText("2.5-3.2", 168F, 329F, textPaintPatient)
+
+
+
+        //Footer1
+        paint.color = getColor(R.color.dark_blue)
+        canvas.drawRect(0F, 370F, 300F, 390F, paint)
+        val xPos1 = (canvas.width/5).toFloat()
+        val textPaint1 = TextPaint()
+        textPaint1.color = getColor(R.color.white)
+        textPaint1.textSize = 6F
+        canvas.drawText("+91 8508092626  |  support@mefy.care  |  CIN No. - 547474684656457", xPos1, 383F, textPaint1)
+
+
+        //Footer2
+        paint.color = getColor(R.color.purple_700)
+        canvas.drawRect(0F, 390F, 300F, 400F, paint)
+        val xPos = (canvas.width/4).toFloat()
+        val textPaint = TextPaint()
+        textPaint.color = getColor(R.color.white)
+        textPaint.textSize = 5.5F
+        canvas.drawText("This Prescription is automatically generated by MeFy Care", xPos, 397F, textPaint)
+
+
+        //Top Border of the Page
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 500F
+        paint.color = getColor(R.color.purple_700)
+        canvas.drawLine(10F, 0F, 10F, 10F, paint)
+
+        pdfDocument.finishPage(myPage1)
+
+        //Writing File to the External Storage
+        if (isExternalStorageWritable()) {
+            val file = File(Environment.getExternalStorageDirectory(), "/NewTest.pdf")
+            try {
+                pdfDocument.writeTo(FileOutputStream(file))
+                pdfDocument.close()
+                Toast.makeText(this, "File is successfully saved ${file.path}", Toast.LENGTH_SHORT)
+                    .show()
+            } catch (e: IOException) {
+                Toast.makeText(this, "File is not saved ${e.localizedMessage}", Toast.LENGTH_SHORT)
+                    .show()
+                e.printStackTrace()
+            }
+        } else {
+            Toast.makeText(this, "Can't save to external storage", Toast.LENGTH_SHORT).show()
+            pdfDocument.close()
+        }
+    }
+
+    private fun isExternalStorageWritable(): Boolean {
+        return Environment.MEDIA_MOUNTED == Environment.getExternalStorageState()
+    }
+}
---
+++ app/src/main/java/com/example/testreport/helper/PermissionHelper.kt
@@ -0,0 +1,174 @@
+package com.example.testreport.helper
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import java.util.ArrayList
+
+class PermissionHelper {
+
+    private val TAG = "Permission Helper"
+    private var REQUEST_CODE: Int = 0
+
+    private var activity: Activity? = null
+    private var fragment: Fragment? = null
+    private var permissions: Array<String>? = null
+    private var mPermissionCallback: PermissionCallback? = null
+    private var showRational: Boolean = false
+
+    //=========Constructors - START=========
+    constructor(activity: Activity, fragment: Fragment, permissions: Array<String>, requestCode: Int) {
+        this.activity = activity
+        this.fragment = fragment
+        this.permissions = permissions
+        this.REQUEST_CODE = requestCode
+        checkIfPermissionPresentInAndroidManifest()
+    }
+
+    constructor(activity: Activity, permissions: Array<String>, requestCode: Int = 100) {
+        this.activity = activity
+        this.permissions = permissions
+        this.REQUEST_CODE = requestCode
+        checkIfPermissionPresentInAndroidManifest()
+    }
+
+    constructor(fragment: Fragment, permissions: Array<String>, requestCode: Int = 100) {
+        this.fragment = fragment
+        this.permissions = permissions
+        this.REQUEST_CODE = requestCode
+        checkIfPermissionPresentInAndroidManifest()
+    }
+
+
+    private fun checkIfPermissionPresentInAndroidManifest() {
+        for (permission in permissions!!) {
+            if (hasPermissionInManifest(permission) == false) {
+                throw RuntimeException("Permission ($permission) Not Declared in manifest")
+            }
+        }
+    }
+
+    fun request(permissionCallback: PermissionCallback?) {
+        this.mPermissionCallback = permissionCallback
+        if (hasPermission() == false) {
+            showRational = shouldShowRational(permissions!!)
+            if (activity != null)
+                ActivityCompat.requestPermissions(activity!!, filterNotGrantedPermission(permissions!!), REQUEST_CODE)
+            else
+                fragment!!.requestPermissions(filterNotGrantedPermission(permissions!!), REQUEST_CODE)
+        } else {
+            Log.i(TAG, "PERMISSION: Permission Granted")
+            mPermissionCallback?.onPermissionGranted()
+            requestAllCallback()
+        }
+    }
+
+    private var requestAllCallback: () -> Unit? = fun() {};
+    fun requestAll(callback: () -> Unit) {
+        this.requestAllCallback = callback;
+        request(null)
+    }
+
+    private var requestIndividualCallback: (grantedPermission: Array<String>) -> Unit? = fun(grantedPermission: Array<String>) {};
+    fun requestIndividual(callback: (grantedPermission: Array<String>) -> Unit) {
+        this.requestIndividualCallback = callback;
+        request(null)
+    }
+
+    private var deniedCallback: (isSystem: Boolean) -> Unit? = fun(isSystem: Boolean) {};
+    fun denied(callback: (isSystem: Boolean) -> Unit) {
+        this.deniedCallback = callback;
+    }
+
+    interface PermissionCallback {
+        fun onPermissionGranted()
+
+        fun onIndividualPermissionGranted(grantedPermission: Array<String>)
+
+        fun onPermissionDenied()
+
+        fun onPermissionDeniedBySystem()
+    }
+
+    private fun <T : Context> getContext(): T? {
+        return if (activity != null) activity as T? else fragment!!.context as T
+    }
+
+    private fun filterNotGrantedPermission(permissions: Array<String>): Array<String> {
+        val notGrantedPermission = ArrayList<String>()
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(getContext()!!, permission) != PackageManager.PERMISSION_GRANTED) {
+                notGrantedPermission.add(permission)
+            }
+        }
+        return notGrantedPermission.toTypedArray()
+    }
+
+    private fun shouldShowRational(permissions: Array<String>): Boolean {
+        var currentShowRational = false
+        for (permission in permissions) {
+
+            if (activity != null) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(activity!!, permission)) {
+                    currentShowRational = true
+                    break
+                }
+            } else {
+                if (fragment!!.shouldShowRequestPermissionRationale(permission)) {
+                    currentShowRational = true
+                    break
+                }
+            }
+        }
+        return currentShowRational
+    }
+
+    fun hasPermission(): Boolean {
+        for (permission in permissions!!) {
+            if (ContextCompat.checkSelfPermission(getContext()!!, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun hasPermissionInManifest(permission: String): Boolean {
+        try {
+            val context = if (activity != null) activity else fragment!!.activity
+            val info = context?.getPackageManager()?.getPackageInfo(context.getPackageName(), PackageManager.GET_PERMISSIONS)
+            if (info?.requestedPermissions != null) {
+                for (p in info?.requestedPermissions) {
+                    if (p == permission) {
+                        return true
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return false
+    }
+
+    fun openAppDetailsActivity() {
+        if (getContext<Context>() == null) {
+            return
+        }
+        val i = Intent()
+        i.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+        i.addCategory(Intent.CATEGORY_DEFAULT)
+        i.data = Uri.parse("package:" + getContext<Context>()!!.packageName)
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+        i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+        getContext<Context>()!!.startActivity(i)
+    }
+
+}
